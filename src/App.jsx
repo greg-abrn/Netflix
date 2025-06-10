1595db19ee3738e68281f05b3d4c21fe22fd47f2
Grégoire Aubertin <greg_abrn@icloud.com>
2025-06-10 16:27:45 +0200

    netflix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import movies from "./assets/movies.json";
+import Section from "./components/Section";
 
 const App = () => {
   return (
@@ -20,7 +21,7 @@ const App = () => {
             <section>
               <h2>{title.category}</h2>
               <div>
-                {title.images.map((url) => {
+                {title.images.map((url, index) => {
                   // console.log(elem); https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642943/react-new-exercices/netflix2022/reprendre%20avec/AAAABd4eOiOZFaZT1-9l68Ra0uC-4kl3zMSPkeA5TyDu8v_KNgAAvifmM8boe5G2kexKSdk2uznXcDIJIlBoz4RA6p-q0_E_xg9maq.webp
                   return (
                     <img src={url} alt="Couverture d'un film ou d'une série" />
