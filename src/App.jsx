ec0ed3f6ac17018f97e5d89d5a3d9b4e59db75fa
Grégoire Aubertin <greg_abrn@icloud.com>
2025-06-10 17:01:20 +0200

    netflix

--- src/App.jsx
+++ src/App.jsx
@@ -17,19 +17,7 @@ const App = () => {
       <main>
         {movies.map((title, index) => {
           // console.log(elem.category); // {category: 'Reprendre avec', images: Array(27)}
-          return (
-            <section>
-              <h2>{title.category}</h2>
-              <div>
-                {title.images.map((url, index) => {
-                  // console.log(elem); https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642943/react-new-exercices/netflix2022/reprendre%20avec/AAAABd4eOiOZFaZT1-9l68Ra0uC-4kl3zMSPkeA5TyDu8v_KNgAAvifmM8boe5G2kexKSdk2uznXcDIJIlBoz4RA6p-q0_E_xg9maq.webp
-                  return (
-                    <img src={url} alt="Couverture d'un film ou d'une série" />
-                  );
-                })}
-              </div>
-            </section>
-          );
+          return <Section key={index} />;
         })}
       </main>
     </>
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,5 +1,17 @@
-const Section = () => {
-  return;
+const Section = (props) => {
+  return (
+    <section>
+      <h2>{props.title.category}</h2>
+      <div>
+        {title.images.map((url, i) => {
+          // console.log(elem); https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642943/react-new-exercices/netflix2022/reprendre%20avec/AAAABd4eOiOZFaZT1-9l68Ra0uC-4kl3zMSPkeA5TyDu8v_KNgAAvifmM8boe5G2kexKSdk2uznXcDIJIlBoz4RA6p-q0_E_xg9maq.webp
+          return (
+            <img key={i} src={url} alt="Couverture d'un film ou d'une série" />
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Section;
